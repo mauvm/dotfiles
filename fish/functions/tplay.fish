192b1f3fcc3cf461a41bbdca7cd3e1fb6e20a0e5
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-03-12 10:46:57 +0100

    Update Fish tplay function.

--- fish/functions/tplay.fish
+++ fish/functions/tplay.fish
@@ -1,6 +1,7 @@
 function tplay
+	# Stream video torrent to VLC with tplay /path/to/file.torrent
 	if test ! -f /usr/local/bin/webtorrent
-npm install -g webtorrent
-end
-webtorrent $argv[1] --vlc -q
+		npm install -g webtorrent
+	end
+	webtorrent $argv[1] --vlc -q
 end
